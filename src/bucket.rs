2be31e3d56139d2976903e3026b9365e673a0490
Feng <857555685@qq.com>
2024-01-03 22:40:02 +0800

    增加命令解析

---
+++ src/bucket.rs
@@ -0,0 +1,17 @@
+use std::panic::panic_any;
+use anyhow::{anyhow, bail, Context, Result};
+use aws_sdk_s3::Client;
+use aws_sdk_s3::error::SdkError;
+use aws_sdk_s3::operation::create_bucket::{CreateBucketError, CreateBucketOutput};
+use aws_sdk_s3::operation::delete_bucket::{DeleteBucketError, DeleteBucketOutput};
+
+
+
+pub async fn create_bucket(client: &Client, bucket_name: &str) -> Result<CreateBucketOutput, SdkError<CreateBucketError>> {
+	// BUILD - aws request
+	client.create_bucket().bucket(bucket_name).send().await
+}
+
+pub async fn delete_bucket(client: &Client, bucket_name: &str) -> Result<DeleteBucketOutput, SdkError<DeleteBucketError>> {
+	client.delete_bucket().bucket(bucket_name).send().await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,114 @@
 pub mod conf;
 pub mod client;
 pub mod object;
+pub mod bucket;
+
+use std::{ffi::{OsString, OsStr}, path::PathBuf};
 
 use anyhow::{Result, Error};
 use config::{Config, File};
 use conf::AppConfig;
+
+// clap 引用
+use clap::{Args, Parser, Subcommand, ValueEnum};
+
+/// A fictional versioning CLI
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "rs3")]
+#[command(about = "aws s3 CLI", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Clones repos
+    #[command(arg_required_else_help = true)]
+    CreateBucket {
+        /// The remote to clone
+        bucket: String,
+    },
+    DeleteBucket {
+        /// The remote to clone
+        bucket: String,
+    },
+    /// Compare two commits
+    Diff {
+        #[arg(value_name = "COMMIT")]
+        base: Option<OsString>,
+        #[arg(value_name = "COMMIT")]
+        head: Option<OsString>,
+        #[arg(last = true)]
+        path: Option<OsString>,
+        #[arg(
+            long,
+            require_equals = true,
+            value_name = "WHEN",
+            num_args = 0..=1,
+            default_value_t = ColorWhen::Auto,
+            default_missing_value = "always",
+            value_enum
+        )]
+        color: ColorWhen,
+    },
+    /// pushes things
+    #[command(arg_required_else_help = true)]
+    Push {
+        /// The remote to target
+        remote: String,
+    },
+    /// adds things
+    #[command(arg_required_else_help = true)]
+    Add {
+        /// Stuff to add
+        #[arg(required = true)]
+        path: Vec<PathBuf>,
+    },
+    Stash(StashArgs),
+    #[command(external_subcommand)]
+    External(Vec<OsString>),
+}
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum ColorWhen {
+    Always,
+    Auto,
+    Never,
+}
+
+impl std::fmt::Display for ColorWhen {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.to_possible_value()
+            .expect("no values are skipped")
+            .get_name()
+            .fmt(f)
+    }
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct StashArgs {
+    #[command(subcommand)]
+    command: Option<StashCommands>,
+
+    #[command(flatten)]
+    push: StashPushArgs,
+}
+
+#[derive(Debug, Subcommand)]
+enum StashCommands {
+    Push(StashPushArgs),
+    Pop { stash: Option<String> },
+    Apply { stash: Option<String> },
+}
+
+#[derive(Debug, Args)]
+struct StashPushArgs {
+    #[arg(short, long)]
+    message: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error>  {
@@ -19,15 +123,82 @@ async fn main() -> Result<(), Error>  {
 
     let s3_client = client::get_aws_client(&config);
 
-    let keys = object::list_keys(&s3_client, "test").await?;
-    println!("List:\n{}", keys.join("\n"));
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::CreateBucket { bucket } => {
+            println!("CreateBucket -> {bucket}");
+            let resp = bucket::create_bucket(&s3_client, &bucket).await;
+            println!("result -> {}", resp.is_ok())
+        }
+        Commands::DeleteBucket { bucket } => {
+            println!("DeleteBucket -> {bucket}");
+            let resp = bucket::delete_bucket(&s3_client, &bucket).await;
+            if resp.is_err(){
+                println!("error -> {:?}", resp.unwrap_err().into_service_error())
+            }
+
+        }
+        Commands::Diff {
+            mut base,
+            mut head,
+            mut path,
+            color,
+        } => {
+            if path.is_none() {
+                path = head;
+                head = None;
+                if path.is_none() {
+                    path = base;
+                    base = None;
+                }
+            }
+            let base = base
+                .as_deref()
+                .map(|s| s.to_str().unwrap())
+                .unwrap_or("stage");
+            let head = head
+                .as_deref()
+                .map(|s| s.to_str().unwrap())
+                .unwrap_or("worktree");
+            let path = path.as_deref().unwrap_or_else(|| OsStr::new(""));
+            println!(
+                "Diffing {}..{} {} (color={})",
+                base,
+                head,
+                path.to_string_lossy(),
+                color
+            );
+        }
+        Commands::Push { remote } => {
+            println!("Pushing to {remote}");
+        }
+        Commands::Add { path } => {
+            println!("Adding {path:?}");
+        }
+        Commands::Stash(stash) => {
+            let stash_cmd = stash.command.unwrap_or(StashCommands::Push(stash.push));
+            match stash_cmd {
+                StashCommands::Push(push) => {
+                    println!("Pushing {push:?}");
+                }
+                StashCommands::Pop { stash } => {
+                    println!("Popping {stash:?}");
+                }
+                StashCommands::Apply { stash } => {
+                    println!("Applying {stash:?}");
+                }
+            }
+        }
+        Commands::External(args) => {
+            println!("Calling out to {:?} with {:?}", &args[0], &args[1..]);
+        }
+    }
+
     
 
-    println!("endpoint_url:{}", config.endpoint_url);
-    println!("region:{}", config.region);
-    println!("access_key:{}", config.access_key);
-    println!("secret_key:{}", config.secret_key);
-    println!("path_style:{}", config.path_style);
+    // let keys = object::list_keys(&s3_client, "test").await?;
+    // println!("List:\n{}", keys.join("\n"));
 
     Ok(())
 }
